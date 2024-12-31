cd701b1acb2ab592f969823bdf4c319dd652551e
baqnngg <il8696546@gmail.com>
2024-12-31 21:59:45 +0900

    jang

--- Assets/Script/Boss/Mythos/Skill/Mythos_Dash.cs
+++ Assets/Script/Boss/Mythos/Skill/Mythos_Dash.cs
@@ -12,6 +12,7 @@ public class Mythos_Dash : MonoBehaviour
     private Vector2 dashDirection = Vector2.zero; // 대쉬 방향
     private Mythos_Move mythosMove;
     private GameObject player;
+    private Mythos mythos;
 
     private bool hasSpawned = false; // 치도리 한 번만 생성하기 위한 플래그
 
@@ -19,6 +20,7 @@ public class Mythos_Dash : MonoBehaviour
     {
         // Move_Mythos 스크립트를 찾고 참조 저장
         mythosMove = GetComponent<Mythos_Move>();
+        mythos = GetComponent<Mythos>();
     }
 
     public void ExecuteDash()
@@ -49,7 +51,8 @@ public class Mythos_Dash : MonoBehaviour
         {
             // 플레이어 위치 기준으로 치도리 생성 위치 계산
             Vector3 spawnDirection = player.transform.position.x < transform.position.x ? Vector3.left : Vector3.right;
-            StartCoroutine(SpawnObject(spawnDirection, 2f));
+            mythos.isMove = false;
+            StartCoroutine(SpawnObject(spawnDirection, 0.5f));
         }
 
         mythosMove.isAttack = false;
@@ -62,6 +65,7 @@ public class Mythos_Dash : MonoBehaviour
         Vector3 spawnPosition = transform.position + direction * distance;
         spawnPosition.y = -1f;
         Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
+        mythos.isMove = true;
     }
 
     // 감지 반경 시각화 (디버그용)
