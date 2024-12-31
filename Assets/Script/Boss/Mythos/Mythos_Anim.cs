3fdfd91f72cd932558d0a6ff4ef4447eed633406
Salkogi07 <kogisal099@gmail.com>
2024-12-31 20:49:13 +0900

    Mythos_anim_edit

--- Assets/Script/Boss/Mythos/Mythos_Anim.cs
+++ Assets/Script/Boss/Mythos/Mythos_Anim.cs
@@ -4,7 +4,7 @@ public class Mythos_Anim : MonoBehaviour
 {
     private Animator animator;
 
-    void Start()
+    void Awake()
     {
         animator = GetComponent<Animator>();
     }
@@ -12,9 +12,11 @@ public class Mythos_Anim : MonoBehaviour
     // 애니메이션 재생 메서드
     public void PlayAnimation(string animationName)
     {
+        Debug.Log(animator);
         if (animator != null)
         {
             animator.Play(animationName);
+            Debug.Log(animationName);
         }
     }
 }
